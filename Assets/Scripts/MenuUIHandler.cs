8fcbc71f552de7b77bd98c3c02f27b8824e3067d
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2023-06-01 02:41:39 -0300

    fix manage scene flow and data
    Implement data persistence between sessions

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -17,6 +17,7 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         MainManager.Instance.TeamColor = color;
+        
     }
 
     private void Start()
@@ -24,6 +25,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -32,11 +34,23 @@ public class MenuUIHandler : MonoBehaviour
     }
     public void QuitGame()
     {
+        MainManager.Instance.SaveColor();
 
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit(); 
 #endif
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
